329a752096bbf1dea7f9435fb4e6231aabd54f28
Minu <46252059+alsdn4956@users.noreply.github.com>
2025-09-15 21:10:22 +0900

    [Silver II] Title: 쇠막대기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,20 @@
+S = input()
+
+stack = []
+count = 0
+
+
+for i in range(len(S)):
+    if S[i] == "(":
+        stack.append("(")
+    
+    else :
+        if S[i-1] == "(":
+            stack.pop()
+            count+= len(stack)
+            
+        else :
+            stack.pop()
+            count+= 1
+            
+print(count)
